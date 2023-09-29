ecaffa2df81fef7035720621956091322fcaace3
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-09-29 12:16:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+// An array of band names
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Led Zeppelin'];
+
+// Function to remove articles from a band name
+function removeArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+// Remove articles and sort the band names
+bandNames = bandNames.map(removeArticles).sort();
+
+// Get the ul element by its ID
+const bandList = document.getElementById('band');
+
+// Create and append list items to the ul element
+bandNames.forEach(function (name) {
+  const li = document.createElement('li');
+  li.textContent = name;
+  bandList.appendChild(li);
+});
+
 
